9001b2e3e6b6c4c851c498bc6b7cd0f620c37ced
Joel Mut <joel.mut@southworks.com>
2020-09-16 16:00:45 -0300

    Adds unit tests for ServiceCollectionExtensions class

---
+++ tests/Bot.Builder.Community.Adapters.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests
+{
+    [Trait("TestCategory", "Twitter")]
+    public class ServiceCollectionExtensionsTests
+    {
+        private static readonly Mock<IServiceCollection> _testService = new Mock<IServiceCollection>();
+        private static readonly Mock<Action<TwitterOptions>> _testAction = new Mock<Action<TwitterOptions>>();
+
+        [Fact]
+        public void AddTwitterAdapterShouldExecuteSuccessfully()
+        {
+            _testService.Setup(e => e.Add(default));
+            _testService.Setup(e => e.GetEnumerator()).Returns(new List<ServiceDescriptor>().GetEnumerator());
+
+            _testService.Object.AddTwitterAdapter(_testAction.Object);
+
+            _testService.Verify(e => e.Add(It.IsAny<ServiceDescriptor>()));
+            _testService.Verify(e => e.GetEnumerator());
+        }
+    }
+}
