06cd5a362431e9d23f5bb19a42f93f25dba36494
CeciliaAvila <cecilia.avila@southworks.com>
2020-09-16 14:56:00 -0300

    Migrate tests to xUnit

--- tests/Bot.Builder.Community.Adapters.Tests/Hosting/WebhookHostingServiceTests.cs
+++ tests/Bot.Builder.Community.Adapters.Tests/Hosting/WebhookHostingServiceTests.cs
@@ -1,34 +1,36 @@
-﻿using Bot.Builder.Community.Adapters.Twitter.Hosting;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Bot.Builder.Community.Adapters.Twitter.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Xunit;
 
 namespace Bot.Builder.Community.Adapters.Twitter.Tests.Hosting
 {
-    [TestClass]
-    [TestCategory("Twitter")]
+    [Trait("TestCategory", "Twitter")]
     public class WebhookHostingServiceTests
     {
         private readonly Mock<ILogger<WebhookHostedService>> _testLogger = new Mock<ILogger<WebhookHostedService>>();
         private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
         private readonly Mock<IApplicationLifetime> _testAppLifetime = new Mock<IApplicationLifetime>();
 
-        [TestMethod]
+        [Fact]
         public void StartAsyncShouldComplete()
         {
             var service = new WebhookHostedService(_testAppLifetime.Object, _testOptions.Object, _testLogger.Object);
             var start = service.StartAsync(default);
-            Assert.IsTrue(start.IsCompleted);
+            Assert.True(start.IsCompleted);
         }
 
-        [TestMethod]
+        [Fact]
         public void StopAsyncShouldComplete()
         {
             var service = new WebhookHostedService(_testAppLifetime.Object, _testOptions.Object, _testLogger.Object);
             var stop = service.StopAsync(default);
-            Assert.IsTrue(stop.IsCompleted);
+            Assert.True(stop.IsCompleted);
         }
     }
 }
