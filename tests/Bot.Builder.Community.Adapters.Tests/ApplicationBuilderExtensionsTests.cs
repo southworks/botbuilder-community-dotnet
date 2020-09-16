1bbd58111f7f9f96bdb6b0c9208b784d18ddc9a4
Joel Mut <joel.mut@southworks.com>
2020-09-16 16:23:14 -0300

    Reorder usings properly

--- tests/Bot.Builder.Community.Adapters.Tests/ApplicationBuilderExtensionsTests.cs
+++ tests/Bot.Builder.Community.Adapters.Tests/ApplicationBuilderExtensionsTests.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Options;
 using Moq;
-using System;
 using Xunit;
 
 namespace Bot.Builder.Community.Adapters.Twitter.Tests
