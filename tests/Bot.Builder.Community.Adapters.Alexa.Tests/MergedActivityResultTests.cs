54986345093ff60adc62bd01d6f7a180e6be2269
Joel Mut <joel.mut@southworks.com>
2020-09-17 14:23:04 -0300

    Add unit tests for MergedActivityResult class

---
+++ tests/Bot.Builder.Community.Adapters.Alexa.Tests/MergedActivityResultTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Bot.Builder.Community.Adapters.Alexa.Core;
+using Microsoft.Bot.Schema;
+using Xunit;
+
+namespace Bot.Builder.Community.Adapters.Alexa.Tests
+{
+    public class MergedActivityResultTests
+    {
+        [Fact]
+        public void MergedActivityResultShouldBeSetSuccessfully()
+        {
+            var alexaRequestMapperOptions = new MergedActivityResult
+            {
+                MergedActivity = new Activity(),
+                EndOfConversationFlagged = true
+            };
+
+            Assert.IsType<Activity>(alexaRequestMapperOptions.MergedActivity);
+            Assert.True(alexaRequestMapperOptions.EndOfConversationFlagged);
+        }
+    }
+}
