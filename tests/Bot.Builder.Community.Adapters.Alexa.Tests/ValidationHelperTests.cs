fed2c07284eb270cf0c96fd1ce4bd57c8fb4bcf1
CeciliaAvila <cecilia.avila@southworks.com>
2020-09-22 17:10:05 -0300

    Apply feedback

--- tests/Bot.Builder.Community.Adapters.Alexa.Tests/ValidationHelperTests.cs
+++ tests/Bot.Builder.Community.Adapters.Alexa.Tests/ValidationHelperTests.cs
@@ -6,6 +6,7 @@ using Alexa.NET.Request;
 using Bot.Builder.Community.Adapters.Alexa.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Moq;
 using Xunit;
 
@@ -16,8 +17,8 @@ namespace Bot.Builder.Community.Adapters.Alexa.Tests
         [Fact]
         public async Task ValidateRequestWithEmptySignatureUrlsShouldReturnFalse()
         {
-            Microsoft.Extensions.Primitives.StringValues signatureChainUrls = string.Empty;
-            Microsoft.Extensions.Primitives.StringValues signatureHeaders = "test-signature-header";
+            StringValues signatureChainUrls = string.Empty;
+            StringValues signatureHeaders = "test-signature-header";
             var request = new Mock<HttpRequest>();
             request.SetupAllProperties();
             request.Setup(req => req.Headers.TryGetValue(AlexaAuthorizationHandler.SignatureCertChainUrlHeader, out signatureChainUrls)).Returns(true);
